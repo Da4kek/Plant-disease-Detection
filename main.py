f412148e8c9f389373f900dfff476247024f0aeb
Dark <anisri477@gmail.com>
2024-04-17 22:42:12 +0530

    updated graph and dir

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import streamlit as st
 import tensorflow as tf
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
+import os 
 
-# Load the model
 model = tf.keras.models.load_model('models/cnn_model.h5')
 
-# Define the class labels
-# Replace 'class3' and 'class4' with your actual class labels
-class_labels = ['black-spot', 'citrus canker', 'fresh', 'grenning']
+class_labels = os.listdir("dataset/train")
 
 
 def main():
@@ -23,24 +22,27 @@ def main():
         st.image(image, caption='Uploaded Image', use_column_width=True)
 
         if st.button('Predict'):
-            # Preprocess the image
             image = image.resize((150, 150))
             image = tf.keras.preprocessing.image.img_to_array(image)
             image = image / 255.0
             image = np.expand_dims(image, axis=0)
 
-            # Make prediction
             prediction = model.predict(image)[0]
 
-            # Determine the predicted class label
             predicted_class_index = np.argmax(prediction)
             predicted_class = class_labels[predicted_class_index]
 
-            # Print the predicted class label and confidence
             st.write(f"Predicted Class: {predicted_class}")
             st.write("Confidence:")
             for i, label in enumerate(class_labels):
                 st.write(f"{label}: {round(prediction[i] * 100, 2)}%")
+            
+            plt.bar(class_labels, prediction * 100)
+            st.set_option("deprecation.showPyplotGlobalUse", False)
+            plt.xlabel('Class Labels')
+            plt.ylabel('Confidence (%)')
+            plt.title('Confidence Scores')
+            st.pyplot()
 
 
 if __name__ == '__main__':
